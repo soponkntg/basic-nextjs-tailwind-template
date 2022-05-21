9981d8da79a496dca7928fd7d92bfb9ab48b732d
soponkntg <sopon-1231@hotmail.com>
2022-05-21 21:59:41 +0700

    add husky

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Nexasdt App</title>
+        <title>Create Next App</title>
 
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -15,7 +15,7 @@ const Home: NextPage = () => {
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
           <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!asdsa
+            Next.js!
           </a>
         </h1>
 
