9e0c4b05a291be8dc54d7de708652b000b15c684
soponkntg <sopon-1231@hotmail.com>
2022-05-21 22:01:51 +0700

    bad commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ const Home: NextPage = () => {
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
           <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
+            Next.js! asdas
           </a>
         </h1>
 
