1d6f39bb7f0d13a57c45f969d6f1f8da1e7238b4
soponkntg <sopon-1231@hotmail.com>
2022-05-21 22:01:32 +0700

    add husky

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ const Home: NextPage = () => {
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
           <a className="text-blue-600" href="https://nextjs.org">
-            Next.js! asds
+            Next.js!
           </a>
         </h1>
 
