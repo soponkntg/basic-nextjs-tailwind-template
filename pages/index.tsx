cf7dd09a24cb84f18b732b85f60a73466ffcec0b
soponkntg <sopon-1231@hotmail.com>
2022-05-21 21:54:12 +0700

    bad commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,8 @@ const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Nextasd App</title>
+        <title>Create Next App</title>
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
